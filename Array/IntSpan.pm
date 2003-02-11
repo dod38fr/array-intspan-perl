085d34eafbb1ded01a65e0b792358deae6f5f6ae
Dominique Dumont <ddumont@cpan.com>
2003-02-11 14:50:52 +0000

    - better set_range

--- Array/IntSpan.pm
+++ Array/IntSpan.pm
@@ -23,6 +23,7 @@
 
 
 use strict;
+use warnings ;
 
 package Array::IntSpan;
 
@@ -40,9 +41,11 @@ sub new {
 sub search {
   my ($self,$start,$end,$index) = @_ ;
 
+  # Binary search for the first element that is *entirely* before the
+  # element to be inserted
   while ($start < $end) {
     my $mid = int(($start+$end)/2);
-    if ($self->[$mid]->[1] < $index) {
+    if ($self->[$mid][1] < $index) {
       $start = $mid+1;
     } else {
       $end = $mid;
@@ -77,7 +80,7 @@ sub check_clobber {
   return @clobbered ;
 }
 
-sub clobbered_items {
+sub get_range {
   my $self = shift;
   my($new_elem) = [@_];
 
@@ -85,11 +88,9 @@ sub clobbered_items {
 
   #Before we binary search, we'll first check to see if this is an append operation
   if ($self->[$end_set][1] < $new_elem->[0]) {
-    return () ;
+    return ref($self)->new() ;
   }
 
-  #Binary search for the first element after the last element that is entirely
-  #before the element to be inserted (say that ten times fast)
   my $start_elem = $self->search(0,$end_set,$new_elem->[0]) ;
 
   # now $start contains the index of the element to create or the
@@ -97,42 +98,40 @@ sub clobbered_items {
 
   #If there is no conflict between this element and the new element, we are home free
   if ($start_elem > $end_set || $new_elem->[1] < $self->[$start_elem][0]) {
-    return ();
+    return ref($self)->new();
   }
 
-  my @clobbered ;
+  my @extracted ;
 
   # check if new element is a subset of start_elem
   if ($self->[$start_elem][0] <= $new_elem->[0] 
-      and $new_elem->[1] <= $self->[$start_elem][1]
-      and $self->[$start_elem][2] ne $new_elem->[2] ) {
-    return ([@$new_elem[0,1],$self->[$start_elem][2]]) ;
+      and $new_elem->[1] <= $self->[$start_elem][1] ) {
+    return ref($self)->new([@$new_elem[0,1],$self->[$start_elem][2]]) ;
   }
 
   #If we are here, we need to test for whether we need to frag the conflicting element
-  if ($self->[$start_elem][0] < $new_elem->[0] 
-      and $self->[$start_elem][2] ne $new_elem->[2] ) {
+  if ($self->[$start_elem][0] < $new_elem->[0] ) {
     # split the old element in 2. The 2nd part starts at the same
     # place than the start of new_elem
-    push @clobbered, [$new_elem->[0], $self->[$start_elem][1], $self->[$start_elem][2]];
+    # TBB dclone objects
+    push @extracted, [$new_elem->[0], $self->[$start_elem][1], $self->[$start_elem][2]];
     $start_elem ++ ;
   }
 
-  return @clobbered if $start_elem > $end_set;
+  return ref($self)->new(@extracted) if $start_elem > $end_set;
 
   #Then we go a searching for where to end this insert
 
   my $end_elem =  $self->search($start_elem,$end_set+1,$new_elem->[1]) ;
-  my $value = $new_elem->[2] ;
 
   #Do a fragmentation check
   my $end_frag ;
   if (defined $self->[$end_elem] 
       and $self->[$end_elem][0] <= $new_elem->[1]
-      and $self->[$end_elem][1] > $new_elem->[1]
-      and $self->[$end_elem][2] ne $value) {
+      and $self->[$end_elem][1] > $new_elem->[1]) {
       # split the old element in 2. The 1st part ends at the same
       # place than the end of new_elem
+    # TBD dclone objects
     $end_frag = [$self->[$end_elem][0],$new_elem->[1], $self->[$end_elem][2]];
     $end_elem -- ;
   }
@@ -140,33 +139,42 @@ sub clobbered_items {
   # if new element does not go over end_elem, skip this one
   $end_elem -- if $self->[$end_elem][0] > $new_elem->[1] ;
 
-  push @clobbered, grep {$_->[2] ne $value} @$self[$start_elem .. $end_elem] ;
+  push @extracted, @$self[$start_elem .. $end_elem] ;
 
-  push @clobbered, $end_frag if defined $end_frag ;
+  push @extracted, $end_frag if defined $end_frag ;
 
   # this is a string comparison. We may want to use a deep data
   # structure comparison like Struct::Compare.
-  return @clobbered ;
+  return ref($self)->new(@extracted) ;
+}
+
+sub clobbered_items {
+  my $self = shift;
+  my($range_start,$range_stop,$range_value) = @_;
+
+  my $item = $self->get_range($range_start,$range_stop) ;
+
+  return   grep {$_->[2] ne $range_value} @$item ;
 }
 
 sub consolidate {
-  my $self = shift;
+  my ($self,$bottom,$top) = @_;
 
-  my @old = @$self;  # backup
-  @$self = () ; # fresh start
+  $bottom = 0 unless defined $bottom ;
+  $top = $#$self unless defined $top;
 
-  my $oldv ;
-  map {
-    if (not defined $oldv or $oldv ne $_->[2])
-      {
-        push @$self, $_ ;
-        $oldv = $_->[2] ;
-      }
-    else
-      {
-        $self->[$#$self][1]=$_->[1] ;
-      }
-  } @old ;
+  #print "consolidate from $bottom to $top\n";
+
+  for (my $i= $top; $i>0; $i--)
+    {
+      if ($self->[$i][2] eq $self->[$i-1][2] and
+          $self->[$i][0] == $self->[$i-1][1]+1 )
+        {
+          #print "consolidate splice ",$i-1,",2\n";
+          splice @$self, $i-1, 2,
+            [$self->[$i-1][0], $self->[$i][1], $self->[$i][2]] ;
+        }
+    }
 
 }
 
@@ -187,27 +195,13 @@ sub set_consolidate_range {
       die "error :".$str ;
     }
 
+  #print "splice $offset,$length\n";
   splice @$self, $offset,$length,@list ;
 
-  # merge above
-  my $above = $offset+ scalar(@list) - 1 ;
-  #print "offset $offset length $length  above $above\n";
-  if ($above < $#$self  # substitution occured before last element
-      and $self->[$above][1] == $self->[$above+1][0] -1 # check for adjacent ranges
-      and $self->[$above][2] eq $self->[$above+1][2])
-    {
-      splice @$self, $above , 2, 
-        [$self->[$above][0], $self->[$above+1][1], $self->[$above][2] ] ;
-    }
-
-  # merge below
-  if ($offset  # there was a subsitution after element 0
-      and $self->[$offset-1][1]+1 == $self->[$offset][0] # check for adjacent ranges
-      and $self->[$offset-1][2] eq $self->[$offset][2])  # check for equal values
-    {
-      splice @$self, $offset-1 , 2, 
-        [$self->[$offset-1][0], $self->[$offset][1], $self->[$offset][2] ] ;
-    }
+  my $b = $offset > 0 ? $offset - 1 : 0 ;
+  my $t = $offset+ @list ;
+  $t = $#$self if $t > $#$self ;
+  $self->consolidate($b , $t ) ;
 
   return $length ? 1 : 0 ;
 
@@ -215,63 +209,54 @@ sub set_consolidate_range {
 
 sub get_splice_parms {
   my $self = shift;
-  my($new_elem) = [@_];
+  my $new_elem = [@_];
 
-  my($start, $end) = (0, $#{$self});
+  my $end_range = $#{$self};
+  my $range_size = @$self ; # nb of elements
 
   #Before we binary search, we'll first check to see if this is an append operation
-  if ($self->[$end]->[1] < $new_elem->[0]) {
-    $start = $end+1;
-  } else {
-    #Binary search for the first element after the last element that is entirely
-    #before the element to be inserted (say that ten times fast)
-    $start = $self->search($start,$end,$new_elem->[0]) ;
+  if ($self->[$end_range][1] < $new_elem->[0]) {
+    return ( $range_size, 0, $new_elem);
+  } 
+
+  # Check for prepend operation
+  if ($new_elem->[1] < $self->[0][0] ) {
+    return ( 0 , 0, $new_elem);
   }
 
-  # now $start contains the index of the element to create or the
-  # first element that will be displaced
+  #Binary search for the first element after the last element that is entirely
+  #before the element to be inserted (say that ten times fast)
+  my $start = $self->search(0,     $range_size,  $new_elem->[0]) ;
+  my $end   = $self->search($start,$range_size,  $new_elem->[1]) ;
 
-  #If there is no conflict between this element and the new element, we are home free
-  if ($start > $#{$self} || $new_elem->[1] < $self->[$start]->[0]) {
-    # we should compare value between $new_elem->[2] and $self->[$start]->[2]
+  my $start_offset = $new_elem->[0] - $self->[$start][0] ;
+  my $end_offset   = defined $self->[$end] ? $new_elem->[1] - $self->[$end][0] : undef ;
 
-    return ( $start, 0, $new_elem);
+  #print "get_splice_parms: start $start, end $end, start_offset $start_offset";
+  #print ", end_offset $end_offset" if defined $end_offset ;
+  #print "\n";
 
-  } else {
-    #If we are here, we need to test for whether we need to frag the conflicting element
-    if ($self->[$start]->[0] < $new_elem->[0]) {
-      # split the old element in 2. The 2nd part starts at the same
-      # place than the start of new_elem
-      splice(@{$self}, $start, 1,
-          [$self->[$start]->[0], $new_elem->[0]-1, $self->[$start]->[2]],
-          [$new_elem->[0], $self->[$start]->[1], $self->[$start]->[2]]);
-      $start++;
-    }
+  my @modified = () ;
 
-    #Then we go a searching for where to end this insert
-    my $start_elem = $start;
-
-    $start =  $self->search($start,$#{$self}+1,$new_elem->[1]) ;
-
-    #Do a fragmentation check
-    if (defined $self->[$start] and $self->[$start]->[0] <= $new_elem->[1]) {
-      if ($self->[$start]->[1] > $new_elem->[1]) {
-        # split the old element in 2. The 1st part ends at the same
-        # place than the end of new_elem
-        splice(@{$self}, $start, 1,
-            [$self->[$start]->[0], $new_elem->[1], $self->[$start]->[2]],
-            [$new_elem->[1]+1, $self->[$start]->[1], $self->[$start]->[2]]);
-      }
-      $start++;
-    }
-
-    #Then we splice in the new element.  If the value is undef, we just remove the old stuff
-    if (defined $new_elem->[2]) {
-      return ($start_elem, $start-$start_elem, $new_elem);
-    } else {
-      return ($start_elem, $start-$start_elem);
-    }
+  #If we are here, we need to test for whether we need to frag the
+  #conflicting element
+  if ($start_offset > 0) {
+    # TBD dclone objects, beware of CMM_SLOT ...
+    push @modified ,[$self->[$start][0], $new_elem->[0]-1, $self->[$start][2]] ;
   }
+
+  push @modified, $new_elem if defined $new_elem->[2] ;
+
+  #Do a fragmentation check
+  if (defined $end_offset 
+      and $end_offset >= 0 
+      and $new_elem->[1] < $self->[$end][1]) {
+    push @modified , [$new_elem->[1]+1, $self->[$end][1], $self->[$end][2]] ;
+  }
+
+  my $extra =  (defined $end_offset and $end_offset >= 0) ? 1 : 0 ;
+
+  return ($start, $end - $start + $extra , @modified);
 }
 
 sub lookup {
@@ -349,44 +334,54 @@ Array::IntSpan - a Module for handling arrays using IntSpan techniques
 
 =head1 DESCRIPTION
 
-C<Array::IntSpan> brings the speed advantages of C<Set::IntSpan> (written by Steven McDougall) to
-arrays.  Uses include manipulating grades, routing tables, or any other situation where you have
-mutually exclusive ranges of integers that map to given values.
+C<Array::IntSpan> brings the speed advantages of C<Set::IntSpan>
+(written by Steven McDougall) to arrays.  Uses include manipulating
+grades, routing tables, or any other situation where you have mutually
+exclusive ranges of integers that map to given values.
 
-C<Array::IntSpan::IP> is also provided with the distribution.  It lets you use IP addresses in any
-of three forms (dotted decimal, network string, and integer) for the indices into the array.  See
-the POD for that module for more information.
+C<Array::IntSpan::IP> is also provided with the distribution.  It lets
+you use IP addresses in any of three forms (dotted decimal, network
+string, and integer) for the indices into the array.  See the POD for
+that module for more information.
 
 =head2 Installation instructions
 
 Standard C<Make::Maker> approach or just copy C<Array/IntSpan.pm> into
-C<site/lib/Array/IntSpan.pm> and C<Array/IntSpan/IP.pm> into C<site/lib/Array/IntSpan/IP.pm>.
+C<site/lib/Array/IntSpan.pm> and C<Array/IntSpan/IP.pm> into
+C<site/lib/Array/IntSpan/IP.pm>.
 
 =head1 METHODS
 
 =head2 new
 
-The C<new> method takes an optional list of array elements.  The elements should be in the form
-C<[start_index, end_index, value]>.  They should be in sorted order and there should be no
-overlaps.  The internal method C<_check_structure> will be called to verify the data is correct.
-If you wish to avoid the performance penalties of checking the structure, you can use
-C<Data::Dumper> to dump an object and use that code to reconstitute it.
+The C<new> method takes an optional list of array elements.  The
+elements should be in the form C<[start_index, end_index, value]>.
+They should be in sorted order and there should be no overlaps.  The
+internal method C<_check_structure> will be called to verify the data
+is correct.  If you wish to avoid the performance penalties of
+checking the structure, you can use C<Data::Dumper> to dump an object
+and use that code to reconstitute it.
 
 =head2 set_range
 
-This method takes three parameters - the C<start_index>, the C<end_index>, and the C<value>.  If
-you wish to erase a range, specify C<undef> for the C<value>.  It properly deals with overlapping
-ranges and will replace existing data as appropriate.  If the new range lies after the last
-existing range, the method will execute in O(1) time.  If the new range lies within the existing
-ranges, the method executes in O(n) time, where n is the number of ranges.  The code is not
-completely optimized and will make up to three calls to C<splice> if the new range intersects with
-existing ranges.  It does not consolidate contiguous ranges that have the same C<value>.
+This method takes three parameters - the C<start_index>, the
+C<end_index>, and the C<value>.  If you wish to erase a range, specify
+C<undef> for the C<value>.  It properly deals with overlapping ranges
+and will replace existing data as appropriate.  If the new range lies
+after the last existing range, the method will execute in O(1) time.
+If the new range lies within the existing ranges, the method executes
+in O(n) time, where n is the number of ranges.  The code is not
+completely optimized and will make up to three calls to C<splice> if
+the new range intersects with existing ranges.  It does not
+consolidate contiguous ranges that have the same C<value>.
 
-If you have a large number of inserts to do, it would be beneficial to sort them first.  Sorting
-is O(n lg(n)), and since appending is O(1), that will be considerably faster than the O(n^2) time
-for inserting n unsorted elements.
+If you have a large number of inserts to do, it would be beneficial to
+sort them first.  Sorting is O(n lg(n)), and since appending is O(1),
+that will be considerably faster than the O(n^2) time for inserting n
+unsorted elements.
 
-The method returns C<0> if there were no overlapping ranges and C<1> if there were.
+The method returns C<0> if there were no overlapping ranges and C<1>
+if there were.
 
 =head2 lookup
 
